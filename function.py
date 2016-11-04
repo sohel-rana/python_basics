3a0d74eeee0f1b4fc5d14426b870d25a3a527f0b
Md. Sohel Rana <sohel023010@gmail.com>
2016-11-05 03:08:22 +0600

    Create function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+def hello(name):
+	print("Hello " + name)
+
+def square(number):
+	return number*number
+
+
+hello("Sohel")
+hello("Rana")
+
+squareTotal = square(10);
+print ("Square of 10 is : "+ str(squareTotal))
