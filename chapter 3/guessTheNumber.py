b344533971317d17d59eba2639672984ac9cd33f
Md Sohel Rana <sohel023010@gmail.com>
2016-11-05 16:44:00 +0600

    added guess the number game script

---
+++ chapter 3/guessTheNumber.py
@@ -0,0 +1,19 @@
+
+import random
+
+print ("I am thinking of a number between 1 and 20.")
+
+numberPicked = random.randrange(1, 20)
+countGuess = 0
+userNumber = 0
+
+while numberPicked != userNumber:
+    countGuess += 1
+    print ("Take a guess : ", end="")
+    userNumber = int(input())
+    if userNumber == numberPicked:
+        print ("Good Job! You guessed my number in " + str(countGuess) + " guesses")
+    elif userNumber < numberPicked:
+        print ("The guess is too low! Try again")
+    elif userNumber > numberPicked:
+        print ("The is too high! Try again")
